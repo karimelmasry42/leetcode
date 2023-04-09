78499abf6fa64a893cccfce9e2030c82d7e6d926
Karim Ossama Elmasry <karimelmasry42@icloud.com>
2023-04-09 19:35:49 +0200

    Optimized 1. Two Sum

--- 1-two-sum.cpp
+++ 1-two-sum.cpp
@@ -1,3 +1,45 @@
+/*
+Solved 9 Apr 2023 by Karim Elmasry
+https://leetcode.com/problems/two-sum/
+Given an array of integers nums and an integer target, return indices of the two
+numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not
+use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time
+complexity?
+
+TIME COMPLEXITY: 7ms, beats 95.65%
+MEMORY: 10.5 MB, beats 48.94%
+*/
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
@@ -9,24 +51,26 @@ public:
     int i = 0, j = NUMS_SIZE - 1;
     while (i <= j) {
       int sum = copy[i] + copy[j];
-      if (sum == target)
+      if (copy[i] + copy[j] == target)
         break;
       else if (sum < target)
         i++;
       else
         j--;
     }
-    int values[] = {copy[i], copy[j]};
     vector<int> ans(2);
-    for (int loop = 0; loop <= 1; loop++) {
-      int prev_i = loop == 0 ? NUMS_SIZE : i;
-      i = NUMS_SIZE;
-      while (--i >= 0)
-        if (values[loop] == nums[i] && i != prev_i) {
-          ans[loop] = i;
-          break;
-        }
-    }
+    int k;
+    for (k = 0; k < NUMS_SIZE; k++)
+      if (copy[i] == nums[k]) {
+        ans[0] = k;
+        break;
+      }
+    int prev_k = k;
+    for (k = 0; k < NUMS_SIZE; k++)
+      if (copy[j] == nums[k] && k != prev_k) {
+        ans[1] = k;
+        break;
+      }
     return ans;
   }
 };
